063b61289c47c1cc44843d472eb6d42f83b12d63
monkz0390 <monkz0390@icloud.com>
2019-09-23 17:18:57 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,25 @@
 #CP LIB set up
-#call neopixel lib
+import time
+from adafruit_circuitplayground.express import cpx
+#call neopixel lib 
 #call speaker lib
 #call accelerometer lib
 
 #number of skin layers
 #heart kill spot
-#
+#pain function
+def pain():
+  print("pain")
+#agony function
+def agony():
+  print("agony")
+#death function
+def death():
+  print("death")
+  
+#main code loop
+while True:
+  
+  print("alive")
+  #normal face
+  
