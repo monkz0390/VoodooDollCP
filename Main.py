b939a875f8a2f62ae9d965c199aaa4ad181e9fb5
monkz0390 <monkz0390@icloud.com>
2019-10-09 16:43:27 -0700

    Update Main.py
    Layer set up pins as inputs still need to test and configure layers to correct pin

--- Main.py
+++ Main.py
@@ -6,7 +6,33 @@ from adafruit_circuitplayground.express import cpx
 #call accelerometer lib
 
 #number of skin layers
-#heart kill spot
+
+  #layer_a set up
+import digitalio 
+import board
+layer_a = digitalio.DigitalInOut(board.BUTTON_A)
+layer_a.direction = digitalio.Direction.INPUT
+
+
+  #layer_b set up
+import digitalio 
+import board
+layer_b = digitalio.DigitalInOut(board.BUTTON_A)
+layer_b.direction = digitalio.Direction.INPUT
+
+  #layer_c set up
+import digitalio 
+import board
+layer_c = digitalio.DigitalInOut(board.BUTTON_A)
+layer_c.direction = digitalio.Direction.INPUT
+
+
+#heart kill spot set up
+import digitalio 
+import board
+heart = digitalio.DigitalInOut(board.BUTTON_A) 
+heart.direction = digitalio.Direction.INPUT
+
 #pain function
 def pain():
   print("pain")
